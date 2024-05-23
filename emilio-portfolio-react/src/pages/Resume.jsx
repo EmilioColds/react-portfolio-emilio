5cd7284c6b928ee4801ec8ed97494dd86d14a6d6
EmilioColds <emiliofriastapia2705@outlook.com>
2024-05-23 11:06:04 -0600

    Initial setup of resume page, need to add detail info

---
+++ emilio-portfolio-react/src/pages/Resume.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Resume() {
+    return (
+        <section>
+            <h2>
+                Resume
+            </h2>
+            <a href='PATH TO RESUME IN PDF' download> Download Resume </a>
+            <ul>
+                <li> Proficiency 1 </li>
+                <li> Proficiency 1 </li>
+                <li> Proficiency 1 </li>
+            </ul>
+        </section>  
+    );
+}
+
+export default Resume;
